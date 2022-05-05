04d91af6e4a7a9b6b04b41ce4a2b4f667186124a
SanngTT <sangtt18@fpt.com.vn>
2022-05-05 09:59:19 +0700

    add new view

--- demoGit/ViewController.swift
+++ demoGit/ViewController.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var view2 = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //init change
+        view.addSubview(view2)
+        view2.frame = CGRect(x: view.center.x, y: view.center.y, width: 200, height: 200)
+        view2.backgroundColor = .red
     }
 
 
