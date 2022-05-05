c220fe447957d9ccb97df9b565c3e4f108911fe8
SanngTT <sangtt18@fpt.com.vn>
2022-05-05 10:34:37 +0700

    master with green

--- demoGit/ViewController.swift
+++ demoGit/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         //init change
         view.addSubview(view2)
         view2.frame = CGRect(x: view.center.x, y: view.center.y, width: 200, height: 200)
-        view2.backgroundColor = .red
+        view2.backgroundColor = .green
         let gesture = UITapGestureRecognizer(target: self, action: #selector(tapView2))
         view2.addGestureRecognizer(gesture)
         view.isUserInteractionEnabled = true
